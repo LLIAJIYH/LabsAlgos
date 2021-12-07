9eaa678487eb0d71d70c5b27e3a0fe91826cf707
Bayram Tagiev <56293189+LLIAJIYH@users.noreply.github.com>
2021-12-07 17:33:55 +0300

    Add files via upload

---
+++ lab05.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Node{
+	string name;
+	int id;
+	Node* next
+}
+
+class List{
+	private:
+		Node *head, *tail;
+		int amount;
+	public:
+		List(){
+			head = nullptr;
+			tail = nullptr;
+			amount = 0;
+		}
+		void add(Node* element){
+			if(head == nullptr){
+				head = element;
+				tail = element;
+				amount += 1;
+			}else{
+				tail->next = element;
+				tail = tail->next;
+			}
+			
+		}
+		void remove(Node* element){
+			Node *temp = head;
+			while(temp->id != element->id){
+				temp = temp->next;
+			}
+			temp->prev->next = temp->next;
+			delete temp;
+		}
+		void sortAlphabet();
+		void sortUp();
+		void sortDown();
+		void switchRing();
+		void switchStart();
+	
+}
+
+int main(){
+	
+}
